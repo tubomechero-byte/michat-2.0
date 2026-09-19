13b6083ea1aa54f70568dfc07c5629cdf4a68670
tubomechero-byte <tubomechero@gmail.com>
2026-09-19 11:44:23 +0200

    Change route handling to use splat parameter

--- server.js
+++ server.js
@@ -2740,7 +2740,7 @@ setInterval(() => {
 // INDEX
 // =====================================================
 
-app.get("*", (req, res, next) => {
+app.get("/{*splat}", (req, res, next) => {
   if (
     req.path.startsWith("/api/")
   ) {
