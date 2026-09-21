f5a229fb78953ad0b29967ad66f40e078c32fe0f
tubomechero-byte <tubomechero@gmail.com>
2026-09-21 17:32:19 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = "michat-v4";
+const CACHE_VERSION = "michat-v7";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
