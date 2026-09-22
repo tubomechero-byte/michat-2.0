f4419873844bc8f221642fb8243cab81f9ec2962
tubomechero-byte <tubomechero@gmail.com>
2026-09-22 19:07:39 +0200

    Update cache version to michat-v22

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = "michat-v21";
+const CACHE_VERSION = "michat-v22";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
