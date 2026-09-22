8ebbe45948be679aad338e2aba10c072c8ad1074
tubomechero-byte <tubomechero@gmail.com>
2026-09-22 18:53:57 +0200

    Update service worker cache version to v20

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = "michat-v19";
+const CACHE_VERSION = "michat-v20";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
