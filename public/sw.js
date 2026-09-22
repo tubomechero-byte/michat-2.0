07d4fa83ff419f376aa5f9e7799d5ddf13c0d0ff
tubomechero-byte <tubomechero@gmail.com>
2026-09-22 18:49:28 +0200

    Update CACHE_VERSION to michat-v19

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = "michat-v16";
+const CACHE_VERSION = "michat-v19";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
