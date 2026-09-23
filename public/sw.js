a7240285502843ae09314bbdd06f5f9089e2acca
tubomechero-byte <tubomechero@gmail.com>
2026-09-23 17:11:05 +0200

    Update service worker cache version to v29

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = "michat-v28";
+const CACHE_VERSION = "michat-v29";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
